91af48c6c79ae2d79056bdfc21928d3b2ff48607
xypro18 <37643655+xypro18@users.noreply.github.com>
2019-12-10 22:52:27 +0000

    Add files via upload

---
+++ 04.Filesystems/solucao_ex2.py
@@ -0,0 +1,22 @@
+source = '/users/python/Documents/PYA001/04.Filesystems/matches_dataset/'
+destination = '/users/python/Documents/PYA001/04.Filesystems/combinado.csv'
+files = []
+
+
+for x in os.walk(source):
+    if len(x[2]) != 0:
+        for item in x[2]:
+            files.append(x[0] + "/" + item)
+
+with open(destination, 'w', encoding="utf-8") as outfile:
+    firstFile = True
+    for fname in files:
+        with open(fname, encoding="utf-8") as infile:
+            firstLine = True
+            for line in infile:
+                if firstFile == True or firstLine == False:
+                    outfile.write(line)
+                firstLine = False
+        
+        firstFile = False
+
